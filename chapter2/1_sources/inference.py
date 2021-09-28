ab754ba03d2d8da77e6fe587ce8334fb544b5e01
EC2 Default User <ec2-user@ip-172-16-48-86.ec2.internal>
2021-09-28 20:16:00 +0000

    adding sources for 1st example

---
+++ chapter2/1_sources/inference.py
@@ -0,0 +1,69 @@
+from transformers import (
+    pipeline,
+    DistilBertTokenizerFast,
+    DistilBertConfig,
+    DistilBertForSequenceClassification,
+)
+import torch
+import json
+
+
+MODEL_NAME = "distilbert-base-uncased"
+NUM_LABELS = 6 # number of categories
+MAX_LENGTH = 512 # max number of tokens model can handle
+
+
+def model_fn(model_dir):
+    """
+    Load required components (model, config and tokenizer) to constuct inference pipeline.
+
+    This method is executed only once when SageMaker starts model server.
+    """
+
+    # If CUDA device is present, then use it for inference
+    # otherwise fallback to CPU
+    device_id = 0 if torch.cuda.is_available() else -1
+
+    tokenizer = DistilBertTokenizerFast.from_pretrained(MODEL_NAME)
+    config = DistilBertConfig()
+    config.num_labels = NUM_LABELS
+
+    model = DistilBertForSequenceClassification.from_pretrained(
+        model_dir, config=config
+    )
+
+    inference_pipeline = pipeline(
+        model=model,
+        task="text-classification",
+        tokenizer=tokenizer,
+        framework="pt",
+        device=device_id,
+        max_length=MAX_LENGTH,
+        truncation=True
+    )
+
+    return inference_pipeline
+
+
+def transform_fn(inference_pipeline, data, content_type, accept_type):
+    """
+    Deserialize inference request payload, run inferenece, and return back serialized response.
+    Note, that currently only JSON is supported, however, this can be extended further as needed.
+
+    This method is executed on every request to SageMaker endpoint.
+    """
+
+    # Deserialize payload
+    if "json" in content_type:
+        deser_data = json.loads(data)
+    else:
+        raise NotImplemented("Only 'application/json' content type is supported.")
+    
+    # Run inference
+    predictions = inference_pipeline(deser_data)
+    
+    # Serialize response
+    if "json" in accept_type:
+        return json.dumps(predictions)
+    else:
+        raise NotImplemented("Only 'application/json' accept type is supported.")
--- chapter2/1_sources/train.py
+++ chapter2/1_sources/train.py
@@ -1,37 +1,114 @@
 import argparse
 import os
 import json
-import transformers
-# import sklearn
+import torch
+import pandas as pd
+from transformers import (
+    DistilBertTokenizerFast,
+    DistilBertForSequenceClassification,
+    DistilBertConfig,
+    TrainingArguments,
+    Trainer,
+)
+
+MODEL_NAME = "distilbert-base-uncased"
+NUM_LABELS = 6
+
+
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item["label"] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+
+def _get_tokenized_data():
+    """
+    Read data from CSV files and tokenize datasets for training
+    """
+    train_dataset = pd.read_csv(
+        os.path.join(os.environ["SM_CHANNEL_TRAIN"], "train_dataset.csv"), header=0
+    )
+    test_dataset = pd.read_csv(
+        os.path.join(os.environ["SM_CHANNEL_TEST"], "test_dataset.csv"), header=0
+    )
+
+    tokenizer = DistilBertTokenizerFast.from_pretrained(MODEL_NAME)
+    train_encodings = tokenizer(
+        train_dataset["data"].to_list(), truncation=True, padding=True
+    )
+    test_encodings = tokenizer(
+        test_dataset["data"].to_list(), truncation=True, padding=True
+    )
+
+    train_enc_dataset = CustomDataset(train_encodings, train_dataset["category_id"])
+    test_enc_dataset = CustomDataset(test_encodings, test_dataset["category_id"])
+
+    return train_enc_dataset, test_enc_dataset
 
 
 def train(args):
-    pass
+    """
+    Instantiate tokenizer, model config, and download pretrained model.
+    After that run training using hyperparameters defined in SageMaker Training job config.
+    If training is succesfull, save trained model.
+    """
+
+    train_enc_dataset, test_enc_dataset = _get_tokenized_data()
+
+    training_args = TrainingArguments(
+        output_dir=os.getenv(
+            "SM_OUTPUT_DIR", "./"
+        ),  # output directory, if runtime is not
+        num_train_epochs=args.epochs,
+        per_device_train_batch_size=args.per_device_train_batch_size,
+        per_device_eval_batch_size=args.per_device_eval_batch_size,
+        warmup_steps=args.warmup_steps,
+        weight_decay=args.weight_decay,
+        logging_steps=args.logging_steps,
+    )
+
+    # Update config for multicategorical task (default is binary classification)
+    config = DistilBertConfig()
+    config.num_labels = NUM_LABELS
+
+    model = DistilBertForSequenceClassification.from_pretrained(
+        MODEL_NAME, config=config
+    )
+
+    trainer = Trainer(
+        model=model,  # model to be trained
+        args=training_args,  # training arguments, defined above
+        train_dataset=train_enc_dataset,  # training dataset
+        eval_dataset=test_enc_dataset,  # evaluation dataset
+    )
+
+    trainer.train()
+
+    # if training is successfuly completed, we save model to SM_MODEL_DIR directory
+    # SageMaker will automatically upload any artifacts in this directory to S3
+    model.save_pretrained(os.environ["SM_MODEL_DIR"])
 
 
-if __name__ =='__main__':
+if __name__ == "__main__":
 
-    print(f"TRANFORMER version: {transformers.__version__}")
-
+    # SageMaker passes hyperparameters  as command-line arguments to the script
+    # Parsing them below...
     parser = argparse.ArgumentParser()
-
-    # hyperparameters sent by the client are passed as command-line arguments to the script.
-    parser.add_argument('--epochs', type=int, default=1)
-    parser.add_argument('--per-device-train-batch-size', type=int, default=16)
-    parser.add_argument('--per-device-eval-batch-size', type=int, default=64)
-
-    parser.add_argument('--logging-steps', type=float, default=100)
-
-
-    # an alternative way to load hyperparameters via SM_HPS environment variable.
-    parser.add_argument('--sm-hps', type=json.loads, default=os.environ['SM_HPS'])
-
-    # input data and model directories
-    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
-    parser.add_argument('--train', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
-    parser.add_argument('--test', type=str, default=os.environ['SM_CHANNEL_TEST'])
+    parser.add_argument("--epochs", type=int, default=1)
+    parser.add_argument("--per-device-train-batch-size", type=int, default=16)
+    parser.add_argument("--per-device-eval-batch-size", type=int, default=64)
+    parser.add_argument("--warmup-steps", type=int, default=100)
+    parser.add_argument("--logging-steps", type=float, default=100)
+    parser.add_argument("--weight-decay", type=float, default=0.01)
 
     args, _ = parser.parse_known_args()
 
-    # ... load from args.train and args.test, train a model, write model to args.model_dir.
     train(args)
