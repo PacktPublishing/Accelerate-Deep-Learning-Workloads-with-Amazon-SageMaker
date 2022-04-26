02951c44526ca02d845d6143ae70e8a27cc64e45
vdabravolski <vdabravolski@bloomberg.net>
2022-04-25 23:51:21 -0400

    resolving issue with batch size, model not learning

--- chapter6/4_sources/train_sm_mp.py
+++ chapter6/4_sources/train_sm_mp.py
@@ -6,6 +6,7 @@ import argparse
 import ast
 import os
 import random
+import logging
 
 import numpy as np
 import smdistributed.modelparallel.torch as smp
@@ -18,12 +19,17 @@ from torch.optim.lr_scheduler import StepLR
 from torchnet.dataset import SplitDataset
 from torchvision import datasets, models, transforms
 
-# Make cudnn deterministic in order to get the same losses across runs.
-# The following two lines can be removed if they cause a performance impact.
-# For more details, see:
-# https://pytorch.org/docs/stable/notes/randomness.html#cudnn
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+NUM_CLASSES = 2  # two classes: ants and bees
+
+## Make cudnn deterministic in order to get the same losses across runs.
+## The following two lines can be removed if they cause a performance impact.
+## For more details, see:
+## https://pytorch.org/docs/stable/notes/randomness.html#cudnn
+# torch.backends.cudnn.deterministic = True
+# torch.backends.cudnn.benchmark = False
 
 
 # SM Distributed: Define smp.step. Return any tensors needed outside.
@@ -35,7 +41,7 @@ def train_step(model, data, target):
     output = model(data)
     # original loss from SM primer
     #     loss = F.nll_loss(output, target, reduction="mean")
-    loss = F.cross_entropy(output, target)
+    loss = F.cross_entropy(output, target, reduction="mean")
 
     # scaled_loss = loss  # TODO: it's not scaling it... need to do something about it
     model.backward(
@@ -44,7 +50,7 @@ def train_step(model, data, target):
     return output, loss
 
 
-def train(model, device, train_loader, optimizer, epoch):
+def train(model, device, train_loader, optimizer, epoch, global_rank):
     model.train()  # set model to training mode
     for batch_idx, (data, target) in enumerate(train_loader):
         # SM Distributed: Move input tensors to the GPU ID used by the current process,
@@ -59,8 +65,8 @@ def train(model, device, train_loader, optimizer, epoch):
 
         optimizer.step()
 
-        if smp.rank() == 0 and batch_idx % 10 == 0:
-            print(
+        if global_rank == 0 and batch_idx % 10 == 0:
+            LOGGER.info(
                 "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
                     epoch,
                     batch_idx * len(data),
@@ -100,91 +106,14 @@ def test(model, device, test_loader):
             correct += sum(correct_batch)
 
     test_loss /= len(test_loader.dataset)
-    if smp.mp_rank() == 0:
-        print(
-            "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
-                test_loss,
-                correct,
-                len(test_loader.dataset),
-                100.0 * correct / len(test_loader.dataset),
-            )
+    LOGGER.info(
+        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss,
+            correct,
+            len(test_loader.dataset),
+            100.0 * correct / len(test_loader.dataset),
         )
-    return test_loss
-
-
-def set_parameter_requires_grad(model, feature_extracting):
-    if feature_extracting:
-        for param in model.parameters():
-            param.requires_grad = False
-
-
-def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
-    # Initialize these variables which will be set in this if statement. Each of these
-    #   variables is model specific.
-    model_ft = None
-    input_size = 0
-
-    if model_name == "resnet":
-        """Resnet18"""
-        model_ft = models.resnet18(pretrained=use_pretrained)
-        set_parameter_requires_grad(model_ft, feature_extract)
-        num_ftrs = model_ft.fc.in_features
-        model_ft.fc = nn.Linear(num_ftrs, num_classes)
-        input_size = 224
-
-    elif model_name == "alexnet":
-        """Alexnet"""
-        model_ft = models.alexnet(pretrained=use_pretrained)
-        set_parameter_requires_grad(model_ft, feature_extract)
-        num_ftrs = model_ft.classifier[6].in_features
-        model_ft.classifier[6] = nn.Linear(num_ftrs, num_classes)
-        input_size = 224
-
-    elif model_name == "vgg":
-        """VGG11_bn"""
-        model_ft = models.vgg11_bn(pretrained=use_pretrained)
-        set_parameter_requires_grad(model_ft, feature_extract)
-        num_ftrs = model_ft.classifier[6].in_features
-        model_ft.classifier[6] = nn.Linear(num_ftrs, num_classes)
-        input_size = 224
-
-    elif model_name == "squeezenet":
-        """Squeezenet"""
-        model_ft = models.squeezenet1_0(pretrained=use_pretrained)
-        set_parameter_requires_grad(model_ft, feature_extract)
-        model_ft.classifier[1] = nn.Conv2d(
-            512, num_classes, kernel_size=(1, 1), stride=(1, 1)
-        )
-        model_ft.num_classes = num_classes
-        input_size = 224
-
-    elif model_name == "densenet":
-        """Densenet"""
-        model_ft = models.densenet121(pretrained=use_pretrained)
-        set_parameter_requires_grad(model_ft, feature_extract)
-        num_ftrs = model_ft.classifier.in_features
-        model_ft.classifier = nn.Linear(num_ftrs, num_classes)
-        input_size = 224
-
-    elif model_name == "inception":
-        """Inception v3
-        Be careful, expects (299,299) sized images and has auxiliary output
-        """
-        model_ft = models.inception_v3(pretrained=use_pretrained)
-        set_parameter_requires_grad(model_ft, feature_extract)
-        # Handle the auxilary net
-        num_ftrs = model_ft.AuxLogits.fc.in_features
-        model_ft.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
-        # Handle the primary net
-        num_ftrs = model_ft.fc.in_features
-        model_ft.fc = nn.Linear(num_ftrs, num_classes)
-        input_size = 299
-
-    else:
-        raise ValueError(f"Invalid model name {model_name}, exiting...")
-        exit()
-
-    return model_ft, input_size
+    )
 
 
 def _cast_dict_from_string(input_dict: dict) -> argparse.Namespace:
@@ -207,69 +136,31 @@ def _cast_dict_from_string(input_dict: dict) -> argparse.Namespace:
     return output_args
 
 
-def main():
-
+def parse_args():
     parser = argparse.ArgumentParser()
 
     # hyperparameters sent by the client are passed as command-line arguments to the script.
     parser.add_argument("--epochs", type=int, default=15)
     parser.add_argument("--batch-size", type=int, default=64)
-    parser.add_argument("--num-classes", type=int, default=2)
-    parser.add_argument("--feature-extract", type=bool, default=True)
-    parser.add_argument("--use-pretrained", type=bool, default=True)
-    parser.add_argument("--sync-s3-path", type=str, default="")
-    parser.add_argument("--model-name", type=str, default="squeezenet")
+    parser.add_argument(
+        "--test-batch-size",
+        type=int,
+        default=1000,
+        metavar="N",
+        help="input batch size for testing (default: 1000)",
+    )
     parser.add_argument(
         "--mp_parameters", type=str, default="", help="Dictionary with SDT parameters"
     )
-    args, unknown = parser.parse_known_args()
+    return parser.parse_known_args()
 
-    print(
-        f"Collected args: {args}. Following args are not parsed and won't be used: {unknown}."
-    )
 
-    # this is parsing MP parameters passed as string by SageMaker
-    # E.g. "--mp_parameters","auto_partition=True,ddp=False,microbatches=8,optimize=speed,partitions=2,pipeline=interleaved,placement_strategy=cluster"
-    sdt_params_strings = dict(item.split("=") for item in args.mp_parameters.split(","))
-    sdt_params = _cast_dict_from_string(sdt_params_strings)
-    print(f"Parsed SDT params are: {sdt_params}")
-
-    if not torch.cuda.is_available():
-        raise ValueError("The script requires CUDA support, but CUDA not available")
-
-    # Initialize the model for this run
-    model, input_size = initialize_model(
-        args.model_name,
-        args.num_classes,
-        args.feature_extract,
-        use_pretrained=args.use_pretrained,
-    )
-    # Print the model we just instantiated
-    print(model)
-
-    # SM Distributed: Set the device to the GPU ID used by the current process.
-    # Input tensors should be transferred to this device.
-    # Fix seeds in order to get the same losses across runs
-    random.seed(1)
-    np.random.seed(1)
-    torch.manual_seed(1)
-    torch.cuda.manual_seed(1)
-
-    smp.init()
-
-    torch.cuda.set_device(smp.local_rank())
-    device = torch.device("cuda")
-    dataloader_kwargs = {"batch_size": args.batch_size}
-    dataloader_kwargs.update(
-        {"num_workers": 1, "pin_memory": True, "shuffle": False, "drop_last": True}
-    )
-
+def get_dataloaders(args, sdmp_args):
     # Data augmentation and normalization for training
-    # Just normalization for validation
     data_transforms = {
         "train": transforms.Compose(
             [
-                transforms.RandomResizedCrop(input_size),
+                transforms.RandomResizedCrop(args.input_size),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
@@ -277,8 +168,8 @@ def main():
         ),
         "val": transforms.Compose(
             [
-                transforms.Resize(input_size),
-                transforms.CenterCrop(input_size),
+                transforms.Resize(args.input_size),
+                transforms.CenterCrop(args.input_size),
                 transforms.ToTensor(),
                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
             ]
@@ -293,51 +184,134 @@ def main():
         for x in ["train", "val"]
     }
 
-    print(f"SMP DP size:{smp.dp_size()}")
-    print(f"SMP DP rank:{smp.dp_rank()}")
-    if (sdt_params.ddp) and smp.dp_size() > 1:
-        partitions_dict = {f"{i}": 1 / smp.dp_size() for i in range(smp.dp_size())}
+    if sdmp_args.dp_size > 1:
+        partitions_dict = {
+            f"{i}": 1 / sdmp_args.dp_size for i in range(sdmp_args.dp_size)
+        }
         dataset_train = SplitDataset(
             image_datasets["train"], partitions=partitions_dict
         )
-        dataset_train.select(f"{smp.dp_rank()}")
-        dataset_val = SplitDataset(image_datasets["val"], partitions=partitions_dict)
-        dataset_val.select(f"{smp.dp_rank()}")
+        dataset_train.select(f"{sdmp_args.dp_rank}")
 
-    elif (not sdt_params.ddp) or smp.dp_size() == 1:
+    elif (not sdmp_args.ddp) or sdmp_args.dp_size == 1:
         dataset_train = image_datasets["train"]
-        dataset_val = image_datasets["val"]
     else:
         raise Exception("Check your distributed configuration.")
 
-    train_loader = torch.utils.data.DataLoader(dataset_train, **dataloader_kwargs)
-    test_loader = torch.utils.data.DataLoader(dataset_val, **dataloader_kwargs)
+    ##    {"num_workers": 1, "pin_memory": True, "shuffle": False, "drop_last": True}
+    train_loader = torch.utils.data.DataLoader(
+        dataset_train,
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=0,
+        pin_memory=True,
+        drop_last=True,
+    )
+
+    test_loader = None
+    # Test loader will be available for global rank 0 process
+    if sdmp_args.rank == 0:
+        test_loader = torch.utils.data.DataLoader(
+            image_datasets["val"],
+            batch_size=args.test_batch_size,
+            shuffle=True,
+            drop_last=True,
+        )
+    return train_loader, test_loader
+
+
+def get_resnet_model():
+    # TODO: change model to resnet.
+    # getting error like below on resnet now:
+    #  on line with relu operation
+    #  result = torch.relu_(input)
+    #  ErrorMessage ":RuntimeError: Output 0 of SMPInputBackward is a view and is being modified inplace.
+    #  This view was created inside a custom Function (or because an input was returned as-is) and the autograd
+    #  logic to handle view+inplace would override the custom backward associated with the custom Function,
+    #  leading to incorrect gradients. This behavior is forbidden. You can fix this by cloning the output of the custom Function.
+    # model_ft = models.resnet18(pretrained=True)
+    model_ft = models.squeezenet1_0(pretrained=True)
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, NUM_CLASSES)
+    input_size = 224  # defined by Resnet18
+
+    return model_ft, input_size
+
+
+def get_squeezenet_model():
+    model_ft = models.squeezenet1_0(pretrained=True)
+    model_ft.classifier[1] = nn.Conv2d(
+        512, NUM_CLASSES, kernel_size=(1, 1), stride=(1, 1)
+    )
+    model_ft.num_classes = NUM_CLASSES
+    input_size = 224
+
+    return model_ft, input_size
+
+
+def main():
+
+    smp.init()
+
+    args, unknown_args = parse_args()
+    # this is parsing MP parameters passed as string by SageMaker
+    # E.g. "--mp_parameters","auto_partition=True,ddp=False,microbatches=8,optimize=speed,partitions=2,pipeline=interleaved,placement_strategy=cluster"
+    sdmp_args_string = dict(item.split("=") for item in args.mp_parameters.split(","))
+    sdmp_args = _cast_dict_from_string(sdmp_args_string)
+    sdmp_args.dp_size = smp.dp_size()
+    sdmp_args.dp_rank = smp.dp_rank()
+    sdmp_args.rank = smp.rank()
+
+    LOGGER.info(
+        f"Collected hyperparameters: {args}. \n Collected Model Parallel parameters: {sdmp_args}.\n"
+        f"Following args are not parsed correctly and won't be used: {unknown_args}."
+    )
+
+    if not torch.cuda.is_available():
+        raise ValueError("The script requires CUDA support, but CUDA not available")
+
+    LOGGER.info(
+        f"Hello from global rank {sdmp_args.rank}.",
+        f"local rank {smp.local_rank()} and local size {smp.local_size()}",
+        f"List of ranks where current model is stored {smp.get_mp_group()}",
+        f"list of ranks with different replicas of the same model {smp.get_dp_group()}",
+        f"current MP rank {smp.mp_rank()} and MP size is {smp.mp_size()}",
+        f"current DP rank {smp.dp_rank()} and DP size is {smp.dp_size()}",
+        f"Other params: {smp.tp_rank()}, {smp.tp_size()}, {smp.pp_rank()}, {smp.pp_size()}, {smp.rdp_rank()}, {smp.rdp_size()}",
+    )
+
+    # TODO: change it
+    # model, args.input_size = get_resnet_model()
+    model, args.input_size = get_squeezenet_model()
+
+    torch.cuda.set_device(smp.local_rank())
+    device = torch.device("cuda")
 
     optimizer = optim.Adadelta(model.parameters(), lr=4.0)
     model = smp.DistributedModel(model)
-    # scaler = smp.amp.GradScaler()  # TODO: remove this as we are not using AMP
     optimizer = smp.DistributedOptimizer(optimizer)
     scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
 
+    # Scaling batch size if data parallelism used
+    if sdmp_args.dp_size > 1:
+        old_batch_size = args.batch_size
+        args.batch_size //= sdmp_args.dp_size
+        args.batch_size = max(args.batch_size, 1)
+        LOGGER.info(f"Scaled batch size from {old_batch_size} to {args.batch_size}.")
+    train_loader, test_loader = get_dataloaders(args, sdmp_args)
+
     for epoch in range(args.epochs):
         # train(model, scaler, device, train_loader, optimizer, epoch) # TODO: clean this
-        train(model, device, train_loader, optimizer, epoch)
-        test_loss = test(model, device, test_loader)
-        print(f"Test loss={test_loss} at {epoch} epoch.")
+        train(model, device, train_loader, optimizer, epoch, sdmp_args.rank)
         scheduler.step()
 
-    # TODO: this needs to be removed.
-    # if smp.rank() == 0:
-    #    if os.path.exists("/opt/ml/local_checkpoints"):
-    #        print("-INFO- PATH DO EXIST")
-    #    else:
-    #        os.makedirs("/opt/ml/local_checkpoints")
-    #        print("-INFO- PATH DO NOT EXIST")
+    if sdmp_args.rank == 0:
+        test(model, device, test_loader)
 
     # Waiting the save checkpoint to be finished before run another allgather_object
     smp.barrier()
 
-    if smp.dp_rank() == 0:
+    if sdmp_args.rank == 0:
         model_dict = model.local_state_dict()
         opt_dict = optimizer.local_state_dict()
         smp.save(
